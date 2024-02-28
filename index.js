64767e3dd8568792b5362c4a75f5dfa641bc01f7
Kotchapun Roekcharoen <kxttpw.j@gmail.com>
2024-02-28 15:42:06 +0700

    save

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const Sequelize = require('sequelize');
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Coffee shop");
+app.use(express.json());
+
+const sequelize = new Sequelize('database','username','password',{
+    host: 'localhost',
+    dialect: 'sqlite',
+    storage: './Database/Coffee.sqlite'
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const Address = sequelize.define('Adress', {
+    
+  tax_id:{
+      type: Sequelize.STRING,
+      autoIncrement: true,
+      primaryKey: true
+  },
+  shop_name:{
+      type: Sequelize.VARCHAR(15),
+      allowNull: false
+  },
+  address_info:{
+      type: Sequelize.VARCHAR(50),
+      allowNull: false
+  },
 });
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}`)); 
