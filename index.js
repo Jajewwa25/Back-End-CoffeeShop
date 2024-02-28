619cb365173d723b7fd887330bf97aefde8ed861
jeynarnice <s6506021621136@email.kmutnb.ac.th>
2024-02-28 16:09:26 +0700

    01

--- index.js
+++ index.js
@@ -52,6 +52,101 @@ const Customer = sequelize.define('Customer', {
     }
   });
 
+  const Employee = sequelize.define('Employee', {
+    
+    employee_id:{
+        type: Sequelize.STRING,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    age:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    position:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    address:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    tel:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    date:{
+        type: Sequelize.DATE,
+        allowNull: false
+    }
+  });
+
+  const Item = sequelize.define('Item', {
+    
+    item_id:{
+        type: Sequelize.STRING,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    item:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    price:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    size:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    date:{
+        type: Sequelize.DATE,
+        allowNull: false
+    }
+  });
+
+  const Order = sequelize.define('Order', {
+    
+    order_id:{
+        type: Sequelize.STRING,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    orderDate:{
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    item_id:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    employee_id:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    customer_id:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    tax_id:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    date:{
+        type: Sequelize.DATE,
+        allowNull: false
+    }
+  });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port http://localhost:${port}`)); 
